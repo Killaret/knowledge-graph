dc8eb397a0f6ee15e4f23826b3a2a094e920c6de
Yaroslav <79209541178@yandex.kz>
2026-04-10 13:34:16 +0300

    fix: remove spread of array inside ESLint config object
    - Remove ...tseslint.configs.recommended from inside files object
    - This was creating numeric keys (0, 1) which ESLint 9 rejects
    - Keep tseslint.configs.recommended at top level of exported array

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -10,17 +10,6 @@ export default [
 		ignores: ['build/', '.svelte-kit/', 'dist/']
 	},
 	{
-		files: ['**/*.js'],
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
-			}
-		}
-	},
-	{
-		files: ['**/*.ts'],
-		...tseslint.configs.recommended,
 		languageOptions: {
 			globals: {
 				...globals.browser,
